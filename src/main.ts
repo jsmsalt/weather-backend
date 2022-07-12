40d7f1b85259d9aa545c68af0e5594c72f1098e1
José Morales <jsmsalt@gmail.com>
2022-07-12 19:57:49 -0300

    feat: Add Helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(helmet());
 
   app.enableVersioning({
     type: VersioningType.URI,
