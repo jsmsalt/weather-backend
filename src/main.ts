db5ea0b8d49f21c52318bc47b89c61250404def6
José Morales <jsmsalt@gmail.com>
2022-07-12 19:56:44 -0300

    feat: Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -10,6 +10,13 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
