fc49677fdedc6851f65a60a634d127ee6d402b75
José Morales <jsmsalt@gmail.com>
2022-07-12 16:00:57 -0300

    feat: Enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
